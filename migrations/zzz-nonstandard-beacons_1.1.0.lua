c792dcd20d797a3628605c5fa31ad2d7dcf7fb48
protocol-1903 <fisher.braydon@yahoo.com>
2025-07-12 17:10:15 -0700

    fix a crash when adding to an existing mod

--- migrations/zzz-nonstandard-beacons_1.1.0.lua
+++ migrations/zzz-nonstandard-beacons_1.1.0.lua
@@ -1,12 +1,14 @@
 for index, metadata in pairs(storage) do
-  local beacon = metadata[1]
-  local source = metadata[2]
-  storage[index] = nil
-  local manager = beacon.surface.create_entity{
-    name = "nsb-internal-manager",
-    position = beacon.position,
-    force = beacon.force
-  }
-  
-  storage[index] = {beacon = beacon, source = source, manager = manager}
+  if index ~= "deathrattles" then
+    local beacon = metadata[1]
+    local source = metadata[2]
+    storage[index] = nil
+    local manager = beacon.surface.create_entity{
+      name = "nsb-internal-manager",
+      position = beacon.position,
+      force = beacon.force
+    }
+    
+    storage[index] = {beacon = beacon, source = source, manager = manager}
+  end
 end
