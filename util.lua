e9e48b772fc3f5162bf75be0a70b3da7e21fd761
protocol_1903 <67478786+protocol-1903@users.noreply.github.com>
2026-01-07 08:08:45 -0700

    fix error and miscalculations in power, switch to lookup table

--- util.lua
+++ util.lua
@@ -1,30 +1,49 @@
 _G.xutil = xutil or {}
 
+local mult_lookup = {
+  int2str = {
+    [0] = "",
+    "k",
+    "M",
+    "G",
+    "T",
+    "P",
+    "E",
+    "Z",
+    "Y",
+    "R",
+    "Q"
+  },
+  str2int = {
+    ["k"] = 3,
+    ["M"] = 6,
+    ["G"] = 9,
+    ["T"] = 12,
+    ["P"] = 15,
+    ["E"] = 18,
+    ["Z"] = 21,
+    ["Y"] = 24,
+    ["R"] = 27,
+    ["Q"] = 30
+  }
+}
+
 xutil.calculate_power = function(energy)
   local exp = ("%e"):format(energy)
   local mult = tonumber(exp:sub(-2))
-  local energy = tonumber(exp:sub(1, -4)) * (10 ^ (mult % 3))
+  local mult2 = mult % 3
+  local energy = tonumber(exp:sub(1, -5)) * (10 ^ (mult2))
+  mult = (mult - mult2) / 3
   -- convert to 000 or 00.0 or 0.0
   local long = energy >= 100
-  return (long and "%d %sW" or "%.1f %sW"):format(energy,
-    mult == 0 and "" or
-    mult <= 1 and "k" or mult <= 2 and "M" or
-    mult <= 3 and "G" or mult <= 4 and "T" or
-    mult <= 5 and "P" or mult <= 6 and "E" or
-    mult <= 7 and "Z" or mult <= 8 and "Y" or
-    mult <= 9 and "R" or mult <= 10 and "Q"
-  )
+  return (long and "%d %sW" or "%.1f %sW"):format(energy, mult_lookup.int2str[mult])
 end
 
 xutil.parse_power = function(energy)
   local mult = not tonumber(energy:sub(1, -2)) and energy:sub(-2, -2) or nil
-  return (mult and energy:sub(1, -3) or energy:sub(1, -2)) * (energy:sub(-1) == "J" and 60 or 1) * 10^(
-    mult == "k" and 3 or mult == "M" and 6 or
-    mult == "G" and 9 or mult == "T" and 12 or
-    mult == "P" and 15 or mult == "E" and 18 or
-    mult == "Z" and 21 or mult == "Y" and 24 or
-    mult == "R" and 27 or mult == "Q" and 30 or 1
-  )
+  return (mult and energy:sub(1, -3) or energy:sub(1, -2)) *
+    (energy:sub(-1) == "J" and 60 or 1) *
+    10 ^ (mult_lookup.str2int[mult] or 0)
 end
 
 return xutil
